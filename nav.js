5bc8a7efac0faefff222b0036bdd654fdf042713
Federico Isla <f.isla@gedis.ar>
2024-12-16 19:32:58 -0300

    intial

---
+++ nav.js
@@ -0,0 +1,11 @@
+function openNav()
+{
+    document.getElementById("mobile-menu").style.width = "40%";
+    document.getElementById("mobile-menu").style.height = "100%";
+    
+}
+
+function closeNav()
+{
+    document.getElementById("mobile-menu").style.width = "0%";
+}
